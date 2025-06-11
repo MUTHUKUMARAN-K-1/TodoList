cc6e9cc5fc7986a633db79306459f9f02611edac
MUTHUKUMARAN-K-1 <muthukumaran.42510@gmail.com>
2025-06-11 22:39:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
 import { useState } from "react";
 import "./App.css";
 import InputContainer from "./components/InputContainer";
-import ToDoContainer from "./components/ToDoContainer";
+import ToDoContainer from "./components/ToDoContainer"; // Make sure the filename matches this import
 
 function App() {
   const [inputVal, setInputVal] = useState('');
   const [todos, setTodos] = useState([]);
+
   function writeTodo(e) {
     setInputVal(e.target.value);
   }
+
   function addTodo() {
-   if (inputVal!='') 
-    {
-        setTodos((prevTodos) => [...prevTodos, inputVal]);
-        setInputVal('');
-      }
+    if (inputVal !== '') {
+      setTodos((prevTodos) => [...prevTodos, inputVal]);
+      setInputVal('');
+    }
   }
 
   console.log(todos);
+
   return (
     <main>
       <h1>To Do list</h1>
       <InputContainer inputVal={inputVal} writeTodo={writeTodo} addTodo={addTodo} />
-
-      <TodoContainer todos={todos}/>   
+      <ToDoContainer todos={todos}/>   
     </main>
   );
 }
